634c523c81ac8b2728a73b7f90e5f8b1b58c6474
IIBystrov <i.i.bystrov@yandex.ru>
2022-12-30 19:58:09 +0300

    Add styles @media 768px (style.css)

--- js/script.js
+++ js/script.js
@@ -2,9 +2,13 @@
 
 let burger = document.querySelector(".burger");
 let burgerLine = document.querySelector(".burger__line");
+let menuContain = document.querySelector(".header__burger-contain");
+let navList = document.querySelector(".nav__list");
 
 burger.addEventListener("click", burgerOpen);
 
 function burgerOpen() {
-    burgerLine.classList.toggle("open");
+    burgerLine.classList.toggle("burger__line--active");
+    menuContain.classList.toggle("burger-contain--active");
+    navList.classList.toggle(".nav__list--active");
 };
