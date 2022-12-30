cb9b49e8ff7caf319c54cbd072b31881a4d7548d
IIBystrov <i.i.bystrov@yandex.ru>
2022-12-30 20:08:58 +0300

    Add styles and script .burger (style.css, script.js)

--- js/script.js
+++ js/script.js
@@ -4,11 +4,13 @@ let burger = document.querySelector(".burger");
 let burgerLine = document.querySelector(".burger__line");
 let menuContain = document.querySelector(".header__burger-contain");
 let navList = document.querySelector(".nav__list");
+let headerBtn = document.querySelector(".header__btn");
 
 burger.addEventListener("click", burgerOpen);
 
 function burgerOpen() {
     burgerLine.classList.toggle("burger__line--active");
     menuContain.classList.toggle("burger-contain--active");
-    navList.classList.toggle(".nav__list--active");
+    navList.classList.toggle("nav__list--active");
+    headerBtn.classList.toggle("header__btn--active");
 };
